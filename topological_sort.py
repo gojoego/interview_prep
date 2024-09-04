752df873c891b368b91bc51257da7c382f0827d3
gojoego <josephjgallego@gmail.com>
2024-09-04 15:39:47 -0500

    "topo sort problems"

--- topological_sort.py
+++ topological_sort.py
@@ -24,6 +24,16 @@ class graph:
             if parent not in self.graph_list:
                 self.graph_list[parent] = []
             self.graph_list[parent].append(child)
+            
+    def initializing_graph(self, dependencies):
+        for x in dependencies:
+            parent, child = x[1], x[0]
+            self.graph[parent], self.graph[child] = [], []
+    
+    def building_graph(self, dependencies):
+        for dependency in dependencies:
+            parent, child = dependency[1], dependency[0]
+            self.graph[parent].append(child)
 
 def find_compilation_order(dependencies):
     sorted_order = [] # store topological order in array 
@@ -63,8 +73,37 @@ def find_compilation_order(dependencies):
     
     return sorted_order # return sorted order list   
     
-     
+from collections import defaultdict, Counter, deque
     
+def alien_order(words):
+    adj_list = defaultdict(set) 
+    counts = Counter({c: 0 for word in words for c in word})
     
+    for word1, word2 in zip(words, words[1:]):
+        for c, d in zip(word1, word2):
+            if c != d:
+                if d not in adj_list[c]:
+                    adj_list[c].add(d)
+                    counts[d] += 1
+                break
+        else: 
+            if len(word2) < len(word1):
+                return ""
     
-    
+    result = []
+    sources_queue = deque([c for c in counts if counts[c] == 0])
+    while sources_queue:
+        c = sources_queue.popleft()
+        result.append(c)
+        for d in adj_list[c]:
+            counts[d] -= 1
+            if counts[d] == 0:
+                sources_queue.append(d)
+    if len(result) < len(counts):
+        return ""
+    return "".join(result)
+    # build graph from input using adjacency lists 
+    # remove sources (vertices with indegree = 0) from graph and add them to results array
+    # decrement indegree of sources children by 1 
+    # repeat until all nodes visited 
+    return result
