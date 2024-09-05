63f6f4859eaebfd33da03c1e9c5cf0fb2440f75a
gojoego <josephjgallego@gmail.com>
2024-09-05 13:40:35 -0500

    more topo problems

--- topological_sort.py
+++ topological_sort.py
@@ -75,35 +75,69 @@ def find_compilation_order(dependencies):
     
 from collections import defaultdict, Counter, deque
     
-def alien_order(words):
+def alien_order(words): 
+    # build graph from input using adjacency lists
+    # step 0: create data structures and find all unique letters 
     adj_list = defaultdict(set) 
-    counts = Counter({c: 0 for word in words for c in word})
+    counts = Counter({c: 0 for word in words for c in word}) # counts/stores number of unique letters
     
-    for word1, word2 in zip(words, words[1:]):
+    # step 1: populate adjacency list and counts 
+    for word1, word2 in zip(words, words[1:]): # for each pair of adjacent words 
         for c, d in zip(word1, word2):
             if c != d:
                 if d not in adj_list[c]:
                     adj_list[c].add(d)
                     counts[d] += 1
                 break
-        else: 
+        else: # check that second word isn't prefix of first 
             if len(word2) < len(word1):
                 return ""
     
+    # step 2: repeatedly pick off nodes with in-degree 0
     result = []
     sources_queue = deque([c for c in counts if counts[c] == 0])
     while sources_queue:
+        # remove sources (vertices with indegree = 0) from graph and add them to results array
         c = sources_queue.popleft()
         result.append(c)
         for d in adj_list[c]:
-            counts[d] -= 1
+            counts[d] -= 1 # decrement indegree of sources children by 1
             if counts[d] == 0:
                 sources_queue.append(d)
-    if len(result) < len(counts):
+        # repeat until all nodes visited 
+        
+    # if not all letters in result, indicates cycle and no valid ordering -> return ""
+    if len(result) < len(counts): 
         return ""
+    
     return "".join(result)
-    # build graph from input using adjacency lists 
-    # remove sources (vertices with indegree = 0) from graph and add them to results array
-    # decrement indegree of sources children by 1 
-    # repeat until all nodes visited 
-    return result
+     
+def verify_alien_dictionary(words, order):
+    # if only one word to check, trivial case with not enough input (2 word min) to run algo, return TRUE
+    if len(words) == 1:
+        return True
+    
+    # store ranking of each letter from order string in data structure 
+    order_map = {} # declare hash map to store characters of words 
+    for index, value in enumerate(order): # traverse order and store rank in order map 
+        order_map[value] = index
+        
+    # iterate over 2 adjacent words in words list 
+    for i in range(len(words) - 1): # traverse words in array
+        for j in range(len(words[i])): # traverse characters in each word 
+            
+            # if all letters have matched so far but current longer than next one, 2 not in order, return False 
+            if j >= len(words[i + 1]): # if words[i + 1] ends before words[i], return FALSE 
+                return False
+            
+            # check if letters in same position in 2 words different 
+            if words[i][j] != words[i + 1][j]:
+                if order_map[words[i][j]] > order_map[words[i + 1][j]]:
+                    return False 
+                break
+    
+    # else, if characters in both words different and words in correct order
+    # exit and move to next 2 adjacent words 
+    # else, return FALSE if characters different in both words and words not in correct order 
+    # at end of loop, all adjacent words have been examined, which ensure that all words sorted, return TRUE 
+    return True
